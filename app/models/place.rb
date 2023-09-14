363536c99076b963857e04964720a38e1aabe71e
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-09-14 16:37:56 +0200

    maj place

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,7 @@ class Place < ApplicationRecord
     #belongs_to :user
 
     validates :title, :description, :price, :category, :address, :zip_code, :city, :borough, presence: true
-    validates :city, format: { with: /\A[a-zA-Z\s]+\z/, message: "ne peut contenir que des lettres et des espaces" }
+    #validates :city, format: { with: /\A[a-zA-Z\s]+\z/, message: "ne peut contenir que des lettres et des espaces" }
     validates :zip_code, length: { is: 5 }
 
 end
