a1e269af2277b93db6685ceeb0d09c1de04aceeb
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-09-18 18:18:04 +0200

    change params places

--- db/migrate/20230912140514_create_places.rb
+++ db/migrate/20230912140514_create_places.rb
@@ -3,13 +3,13 @@ class CreatePlaces < ActiveRecord::Migration[7.0]
     create_table :places do |t|
       t.string :title
       t.text :description
-      t.integer :price
+      t.string :price
       t.string :category
       t.string :image
       t.string :address
       t.integer :zip_code
       t.string :city
-      t.integer :borough
+      t.string :borough
       t.string :url
 
       t.timestamps
