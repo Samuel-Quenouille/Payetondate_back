17af6c834703e08f16679279f9d32cd432c553cc
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-09-25 12:19:42 +0200

    add change places_controller

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,6 @@ class Place < ApplicationRecord
     belongs_to :user
 
     validates :title, :description, :price, :category, :address, :zip_code, :city, :borough, presence: true
-    #validates :city, format: { with: /\A[a-zA-Z\s]+\z/, message: "ne peut contenir que des lettres et des espaces" }
-    #validates :zip_code, length: { is: 5 }
+    validates :zip_code, length: { is: 5 }
 
 end
